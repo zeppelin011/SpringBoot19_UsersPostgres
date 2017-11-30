a5f51c07d0a78165d2d99bda3013098466f0d7c3
GBTC408012UR <GBTC408012UR@MC.MONTGOMERYCOLLEGE.EDU>
2017-11-29 21:23:11 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserService userService;
+
+
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping("/secure")
+    public String secure(){
+        return "secure";
+    }
+
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model){
+        model.addAttribute("user",new User());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult result, Model model){
+        model.addAttribute("user",user);
+
+        if(result.hasErrors()){
+            return "registration";
+        }else{
+            userService.saveUser(user);
+            System.out.println(user.toString());
+            System.out.println("user account created");
+
+
+        }
+
+        return "index";
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/SSUserDetailsService.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.HashSet;
+import java.util.Set;
+
+@Transactional
+@Service
+public class SSUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public SSUserDetailsService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+
+        try {
+            User user = userRepository.findByUsername(username);
+            if (user == null)
+            {
+                return null;
+            }
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    getAuthorities(user));
+        }
+        catch (Exception e)
+        {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+
+    private Set<GrantedAuthority> getAuthorities(User user){
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();//Is hashset because is unique
+        for(Role role : user.getRoles())
+        {
+            System.out.println("roles are " + role.getRole());
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(role.getRole());
+            authorities.add(grantedAuthority);
+        }
+
+        System.out.println("authorities are "  + authorities.toString());
+        return authorities;
+    }
+}
